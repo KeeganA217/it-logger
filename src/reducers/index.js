753c6a64c8f3239e52363bb40a205f4ff92515e2
Keegan <keegan.adams15@gmail.com>
2020-10-11 16:49:34 -0500

    project completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import logReducer from "./logReducer";
+import techReduer from "./techReducer";
 
 export default combineReducers({
   log: logReducer,
+  tech: techReduer,
 });
